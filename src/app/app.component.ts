8d3ae6fcafe13ce8192b516c381ea6e095964674
ruben <a20gazaru@iesgrancapitan.org>
2021-02-03 18:13:57 +0100

    hola mundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   title = 'miprimeraApp';
+  saludo = 'Hola mundo';
 }
